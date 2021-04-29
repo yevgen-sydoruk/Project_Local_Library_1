a35514a7e370ab441536d55c29a0798ac261fc0f
yevgen-sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2021-04-28 21:47:24 -0500

    Books.js
    Functions findBookById and
     partitionBooksByBorrowedStatus Done

--- public/src/books.js
+++ public/src/books.js
@@ -2,9 +2,29 @@ function findAuthorById(authors, id) {
     return authors.find((author) => author.id === id);
 }
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+    return books.find((book) => book.id === id);
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+    let borrowedBooks = [];
+    let returnedBooks = [];
+    let sortedArray = [];
+    books.filter((book) => {
+        // console.log(book);
+        let isBorrowed = book.borrows;
+
+        isBorrowed.find((status) =>
+            status.returned
+                ? returnedBooks.push(book)
+                : borrowedBooks.push(book)
+        );
+    });
+    sortedArray.push(borrowedBooks);
+    sortedArray.push(returnedBooks);
+    // console.log(sortedArray);
+    return sortedArray;
+}
 
 function getBorrowersForBook(book, accounts) {}
 
