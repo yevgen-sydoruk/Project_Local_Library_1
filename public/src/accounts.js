9a704ca6b0a709e4804330eb084248511cc9a295
yevgen-sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2021-04-28 20:00:29 -0500

    Account page
    Functions findAccountById and sortAccountsByLastName are done without problems

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,6 +1,14 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+    return accounts.find((account) => account.id === id); //at least one arrow function + find() native array method
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+    return accounts.sort((accountA, accountB) =>
+        accountA.name.last.toLowerCase() < accountB.name.last.toLowerCase()
+            ? -1
+            : 1
+    ); //ternary operator + sort() native arrow method
+}
 
 function getTotalNumberOfBorrows(account, books) {}
 
